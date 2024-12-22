c1d319cdea92f0137c2c8908d8d3129c0a10e3a6
Veljko03 <veljkodovragovic@gmail.com>
2024-12-22 13:20:13 +0100

    setting up node app

---
+++ app.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("hello"));
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
