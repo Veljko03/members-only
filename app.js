ace5d914f42207e1ba98ad4cd65e3171ed1d972d
Veljko03 <veljkodovragovic@gmail.com>
2024-12-23 13:15:26 +0100

    added form and sing in , added passport config

--- app.js
+++ app.js
@@ -2,59 +2,34 @@ require("dotenv").config();
 const express = require("express");
 const path = require("node:path");
 const session = require("express-session");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcryptjs");
+
+const pool = require("./db/pool");
 
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
-app.use(passport.session());
-app.use(express.urlencoded({ extended: false }));
-
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const { rows } = await pool.query(
-        "SELECT * FROM users WHERE username = $1",
-        [username]
-      );
-      const user = rows[0];
-
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" });
-      }
-      const match = await bcrypt.compare(password, user.password);
-      if (!match) {
-        // passwords do not match!
-        return done(null, false, { message: "Incorrect password" });
-      }
-      return done(null, user);
-    } catch (err) {
-      return done(err);
-    }
+app.use(
+  session({
+    store: new (require("connect-pg-simple")(session))({
+      pool: pool,
+    }),
+    secret: "cat",
+    resave: false,
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      secure: false,
+      httpOnly: true,
+    },
+    saveUninitialized: false,
   })
 );
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
+require("./config/passport");
 
-passport.deserializeUser(async (id, done) => {
-  try {
-    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
-      id,
-    ]);
-    const user = rows[0];
-
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
-});
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.send("hello"));
 
